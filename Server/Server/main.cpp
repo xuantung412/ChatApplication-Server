94294408939763d5129d13f2be139f212f0e48dd
xuantung412 <xuan.tung.nguyen811@gmail.com>
2022-04-26 12:48:01 +0930

    Update new features for server- word suggestion base on threads

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -4,9 +4,88 @@
 #include <sstream>
 #include "UserAccount.cpp"
 #include <vector>
+#include<thread>
+
+using namespace std::this_thread;
+
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
+
+// A job of thread will be processed in here
+	string performTask(string word)
+	{
+		string bestSimilarWord = "no suggestion";
+		int bestCharSimilar = 0;
+
+		string line;
+		ifstream myfile("wordDictionary.txt");
+
+		if (myfile.is_open())
+		{
+			while (myfile.good())
+			{
+				//Get each line in directory
+				getline(myfile, line);
+				//Compare length of misspelt word and selected line. Accept table length is -1,0,+1. To prevent too long/too short misspelt word.
+				if ((line.length() == word.length()) || ((line.length() + 1) == word.length()) || ((line.length() - 1) == word.length())) {
+					//Compare first char of both words
+					if (line.substr(0, 1).compare(word.substr(0, 1)) == 0) {
+						int similarChar = 0;
+						string copyWord = line;
+						for (int i = 0; i < word.length(); i++) {
+							if (copyWord.find(word.substr(i,(i+1))) != std::string::npos) {
+								//cout << copyWord <<"-"<<word.substr(i,(i+1))<<"-" << copyWord.find_first_of(word.substr(i, (i+1))) << endl;
+								int a = copyWord.find_first_of(word.substr(i, (i+1)));
+								copyWord.erase( a, (a+1));
+								similarChar++;
+
+							}
+						}
+						if (similarChar > bestCharSimilar) {
+							bestSimilarWord = line;
+							bestCharSimilar = similarChar;
+						}
+
+					}
+
+				}
+			}
+			myfile.close();
+			//cout << bestSimilarWord<<"-"<<bestCharSimilar << endl;
+		}
+
+		else cout << "Unable to open file";
+		return bestSimilarWord;
+	}
+
+	void performThread(string listOfWord,int fromLine, int toLine,int threadNumber) {
+		//Write to file
+		ofstream myfile;
+		string fileName = "ThreadNumber" + to_string(threadNumber) + ".txt";
+		
+		myfile.open(fileName);
+		for (int i = fromLine; i < toLine+1; i++){
+			stringstream ss(listOfWord);
+			int currentLine = 1;
+			for (std::string line; std::getline(ss, line); )
+			{
+				currentLine++;
+				if (currentLine == i) {
+					string suggestionWord = performTask(line);
+					myfile << suggestionWord << endl;
+				}
+			}
+
+		}
+
+		//Write databack
+
+		//myfile << output;
+		myfile.close();
+
+	}
+
 
 int main()
 {
@@ -67,7 +146,6 @@ int main()
 		// Loop through all the current connections / potential connect
 		for (int i = 0; i < socketCount; i++)
 		{
-			// Makes things easy for us doing this assignment
 			SOCKET sock = copy.fd_array[i];
 
 			// Is it an inbound communication?
@@ -113,7 +191,7 @@ int main()
 					//Create clone UserAccount to call static method to check account.
 						UserAccount newAccount(userName, password);
 						string loginResult = "";
-
+						/*
 						//send to client to accept
 						if (UserAccount::accountLoggin(newAccount)) {
 							//Succesfull register account
@@ -122,6 +200,10 @@ int main()
 						else {
 							loginResult = "/RejectLoggin";
 						}
+						*/
+						//By pass login
+						loginResult = "/AcceptLoggin";
+
 
 						//Loop through all connections in master to find the sent message client to send message back to it.
 						SOCKET clientSock;
@@ -173,6 +255,105 @@ int main()
 					}
 					send(clientSock, registrationResult.c_str(), registrationResult.size() + 1, 0);
 				}
+				else if (completeMessage.find("/File") != std::string::npos) {
+					//get Number of thread:
+					int totalThreads = 1;	//Default threads
+					totalThreads = (int)completeMessage[0];	//Get total threads from client
+					try {	//Check incase invalid threads. If Invalid
+						cout << completeMessage[0] << endl;
+						totalThreads =completeMessage[0] -48;	//Convert string to int
+					}
+					catch (const std::exception& e) {
+						cout << "invalid number of thread from client. Use only 1 thread" << endl;
+					}
+
+					completeMessage = completeMessage.erase(0, 8);	//Remove some system-syntax symbol.
+
+
+					//Write to local file as backup
+					ofstream myfile;
+					myfile.open("backup.txt");
+
+
+					//convert to lower string
+					for (int i = 0; i < completeMessage.length(); i++)
+						completeMessage[i] = tolower(completeMessage[i]);
+					//Get total line and save data to local file to backupfile
+					int totalLine = 0;
+					stringstream ss(completeMessage);
+					for (std::string line; std::getline(ss, line); )
+					{
+						myfile << line;
+						totalLine ++;
+					}
+					myfile.close();
+
+
+
+					//Divine job to each threads. Job each thread= TotalLine % total threads. And the last thread take the reminder Line
+					int jobForThread = (totalLine -(totalLine % totalThreads))/totalThreads;
+					//cout <<totalLine<<"-"<<totalThreads<<"-" << jobForThread << endl;
+					//AssignJobTo Threads.
+					int lineNumber = 1;
+					int totalAssignedLine = 0;
+					for (int i = 0; i < totalThreads; i++) {
+						lineNumber += jobForThread;
+						if (i != totalThreads - 1) {
+							thread th1(performThread, completeMessage, i * jobForThread + 1, (i + 1) * jobForThread,i+1);
+							th1.detach();
+
+						}
+						else {
+							//Last threads will take reminder
+							thread th1(performThread, completeMessage, i* jobForThread + 1, (i + 1)* jobForThread+ (totalLine % totalThreads)+1,i+1);
+							cout << "Wait for all threads complete" << endl;
+							th1.join();	//The program will wait the last thread executated to continue
+							//Last thread took the most jobs so the system just need to wait the last threads complete jobs <-> all threads complete.
+
+						}
+					}
+					cout << "All threads completed" << endl;
+					//Wait for the last thread finish
+					boolean treadhFinishJob = false;
+					while (!treadhFinishJob) {
+						ifstream lastThreadFileOutPut;
+						string fileName = "ThreadNumber" + to_string(totalThreads) + ".txt";
+						lastThreadFileOutPut.open(fileName);
+
+							treadhFinishJob = true;
+							//Merge all outfile from threads:
+							ofstream outputFile;
+							remove("output.txt");
+							outputFile.open("output.txt");
+							string outputFileData = "/WordSuggestionOutput\\n";
+							for (int i = 1; i < totalThreads+1; i++) {
+								string threadOutputFileName = "ThreadNumber" + to_string(i) + ".txt";
+								ifstream fromThreadOutputFile(threadOutputFileName);
+								string lineData;
+								while (std::getline(fromThreadOutputFile, lineData))
+								{
+									outputFileData += lineData + "-";
+									outputFile << lineData << endl;
+								}
+
+								fromThreadOutputFile.close();
+							}
+							outputFile.close();
+
+							//Send output file to all clients
+							cout << "Sending Output to Client..." << endl;
+									ostringstream ss;
+									ss << outputFileData << "\r\n";
+									string strOut = ss.str();
+									send(sock, strOut.c_str(), strOut.size() + 1, 0);
+									cout << "Sent completed." << endl;
+
+
+					}
+
+
+
+				}
 
 				if (bytesIn <= 0)
 				{
@@ -180,7 +361,6 @@ int main()
 					closesocket(sock);
 					FD_CLR(sock, &master);
 				}
-				//From there, Message is not a command. It is a normal message from client to send to other clients.
 
 				else
 				{
@@ -206,7 +386,7 @@ int main()
 						SOCKET outSock = master.fd_array[i];
 						if (outSock != listening && outSock != sock && (completeMessage.find("/") == std::string::npos))
 						{
-
+							cout << "a" << endl;
 							ostringstream ss;
 							ss<< completeMessage << "\r\n";
 							string strOut = ss.str();
@@ -242,7 +422,7 @@ int main()
 	// Cleanup winsock
 	WSACleanup();
 
-	system("pause");
+	//system("pause");
 	return 0;
 }
 
